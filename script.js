b4dc666f248cce86330da9eca21cdb2d509706e0
Janet_Mensah <janetmensah60@gmail.com>
2023-11-27 17:01:37 +0000

    Refactored code in order to make sure that each list has a delete button next to it

--- script.js
+++ script.js
@@ -2,30 +2,35 @@ const buttonAction = document.querySelector('#enter');
 const todoList = document.querySelector('#list');
 const deleteAction = document.querySelector('#delete');
 const dateAction = document.querySelector('#show-date');
+const formAction = document.querySelector('#todo-form');
 
 
-buttonAction.addEventListener('click', (event) => {
+formAction.addEventListener('click', (event) =>{
     event.preventDefault();
-    const listItem = document.createElement('li');
-    const input = document.querySelector('#new-todo');
-    listItem.innerText = input.value;
-    todoList.appendChild(listItem);
-    console.log(listItem);
-    console.log(todoList);
+    const listItem = document.createElement('li'); // created list item
+    const input = document.querySelector('#new-todo'); //created input
+    listItem.innerText = input.value; //made list item equal to user input
+    todoList.appendChild(listItem); //add this item to the todo list
+
+
+    const deleteButton = document.createElement('button'); //create a delete button
+    deleteButton.innerText = 'delete'; //we wrote the delete button name
+    listItem.appendChild(deleteButton); //add the delete button to the list item
+
+
+    deleteButton.addEventListener('click', () =>{
+    todoList.removeChild(listItem); //function to delete the item
+    });
 });
 
-deleteAction.addEventListener('click', (event) =>{
-    event.preventDefault();
-    todoList.removeChild(todoList.lastChild);
-    console.log(todoList);
-});
 
-dateAction.addEventListener('click', (event) => {
+    dateAction.addEventListener('click', (event) => {
     event.preventDefault();
     const currentDate = new Date();
     const currentDateTime = currentDate.toLocaleString();
     console.log(currentDate);
-});
+    });
+
 
 
 
