a33603878b4ececb10f0f6073a78582d8d57b3c1
RyanNekadio <ryanekadio12@hotmail.co.uk>
2023-11-27 15:41:40 +0000

    Added Show Date button and functionality to display current date & time

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const buttonAction = document.querySelector('#enter');
 const todoList = document.querySelector('#list');
 const deleteAction = document.querySelector('#delete');
+const dateAction = document.querySelector('#show-date');
 
 
 buttonAction.addEventListener('click', (event) => {
@@ -17,6 +18,13 @@ deleteAction.addEventListener('click', (event) =>{
     event.preventDefault();
     todoList.removeChild(todoList.lastChild);
     console.log(todoList);
+});
+
+dateAction.addEventListener('click', (event) => {
+    event.preventDefault();
+    const currentDate = new Date();
+    const currentDateTime = currentDate.toLocaleString();
+    console.log(currentDate);
 });
 
 
